175190202a642b151f78ff46cd6406c93b6b9582
Douglas Moreira Santiago <02362480143@mma.gov.br>
2019-06-07 12:00:42 -0300

    Iniciando Projeto em Java Desk

---
+++ src/Classes/DadosLog.java
@@ -0,0 +1,13 @@
+
+package Classes;
+
+
+public class DadosLog {
+    public boolean validarUsuarios(String usuario, String senha){
+        if (usuario.equals("douglas")&& senha.equals("132512")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/Classes/ProjetoJava.java
@@ -0,0 +1,15 @@
+
+package Classes;
+
+import Formulários.frmLogin;
+
+public class ProjetoJava {
+
+    
+    public static void main(String[] args) {
+        frmLogin Login = new frmLogin();
+        Login.setLocationRelativeTo(null);
+        Login.setVisible(true);
+    }
+    
+}
